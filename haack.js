e8cab9ffcbd7b8a00b204ec2912296082aad6a65
bstrp <56723114+bstrp@users.noreply.github.com>
2019-11-25 11:18:18 +0530

    updated

--- haack.js
+++ haack.js
@@ -1,8 +1,2 @@
-var
-khatrimazafull = "<a href='https://khatrimazafull.tw' target='_blank'>khatrimazafull</a>", 
-ninexmovies = "<a href='https://9xmovies.co7.in' target='_blank'>9xmovies</a>",
-bobmovies = "<a href='https://bobmovies.biz' target='_blank'>BobMovies</a>";
-
-document.getElementById('khatrimazafull').innerHTML = khatrimazafull;
-document.getElementById('ninexmovies').innerHTML = ninexmovies;
-document.getElementById('bobmovies').innerHTML = bobmovies;
+var person = "John Doe";
+document.getElementById("demo").innerHTML = person;
