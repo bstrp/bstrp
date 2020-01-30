8cb52c7da7640973ec34d223d4f9c6c4ec1caf49
bstrp <56723114+bstrp@users.noreply.github.com>
2020-01-30 12:38:39 +0530

    upload js

---
+++ test.js
@@ -0,0 +1,6 @@
+var my_2d = [
+  ['https://www.9xmovies.com', '9xmovies', 'download movies from 9xmovies'],
+  ['https://www.amazon.com', 'amazon', 'amazon shoping'],
+  ['https://www.ebay.com', 'ebay', 'ebay shoping'],
+  ['http://www.snapdeal.com', 'snapdeal official', 'snapdeal shooping']
+];
