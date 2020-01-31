7928fd90b0d55b91bce5e19ef63980ed1cfbb8bd
bstrp <56723114+bstrp@users.noreply.github.com>
2020-01-31 12:08:02 +0530

    updated

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var my_2d = [
-  ['https://www.9xmovies.com', '9xmovies', 'download movies from 9xmovies'],
-  ['https://www.amazon.com', 'amazon', 'amazon shoping'],
-  ['https://www.ebay.com', 'ebay', 'ebay shoping'],
-  ['http://www.snapdeal.com', 'snapdeal official', 'snapdeal shooping']
+  [&#39;https://9xmovies.group/&#39;, &#39;9xmovies&#39;, &#39;download movies from 9xmovies&#39;],
+  [&#39;https://khatrimazafull.li/&#39;, &#39;khatrimazafull&#39;, &#39;khatrimazafull&#39;],
+  [&#39;https://moviesflix.biz/&#39;, &#39;moviesflix&#39;, &#39;moviesflix&#39;],
+  [&#39;https://yts.tl/&#39;, &#39;yts&#39;, &#39;yts&#39;]
 ];
