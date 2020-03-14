f420473140dbba13f0411c8a81570bc8c03fc5d6
Madhav <40449711+MpateLhack@users.noreply.github.com>
2020-03-14 10:53:12 +0530

    dltd updt

--- test.js
+++
@@ -1,6 +0,0 @@
-var my_2d = [
-  [&#39;https://9xmovies.group/&#39;, &#39;9xmovies&#39;, &#39;download movies from 9xmovies&#39;],
-  [&#39;https://khatrimazafull.li/&#39;, &#39;khatrimazafull&#39;, &#39;khatrimazafull&#39;],
-  [&#39;https://moviesflix.biz/&#39;, &#39;moviesflix&#39;, &#39;moviesflix&#39;],
-  [&#39;https://yts.tl/&#39;, &#39;yts&#39;, &#39;yts&#39;]
-];
